9147322bc255951bfd427436161009d2539d1027
Stefan Dzisiewski-Smith <stefan@bareconductive.com>
2015-11-06 10:23:01 +0000

    Another small tweak to Compiler_Errors.h

--- Midi_interface/Compiler_Errors.h
+++ Midi_interface/Compiler_Errors.h
@@ -28,7 +28,7 @@
 	  #error Please upgrade your Arduino IDE to 1.6.6 or greater
 	#else 
 		// check that are Conductive Touch Board (USB MIDI, iPad compatible) is selected in Tools -> Board
-		#ifndef ARDUINO_AVR_BARETOUCH
+		#if !defined(ARDUINO_AVR_BARETOUCH) || !defined(IPAD_COMPAT)
 		 	#error Please select "Bare Conductive Touch Board (USB MIDI, iPad compatible)" in the Tools -> Board menu.
 		#endif
  	#endif
